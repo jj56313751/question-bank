ba727d897c261cf729bddb8ee67c06ea9a371799
jj56313751 <120015396@qq.com>
2024-06-11 11:29:09 +0800

    chore: add husky lint-stage

--- app/dashboard/operate/page.tsx
+++ app/dashboard/operate/page.tsx
@@ -1,18 +1,10 @@
-
-
-import { Suspense } from 'react';
-
-import { Metadata } from 'next';
+import { Suspense } from 'react'
+import { Metadata } from 'next'
 
 export const metadata: Metadata = {
   title: '操作台',
-};
+}
 
 export default async function Page() {
-  
-  return (
-    <main>
-      operate
-    </main>
-  );
+  return <main>operate</main>
 }
