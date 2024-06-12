87ec2cc24b0a1f5f11fb762257dbcba262d8d8d9
jj56313751 <120015396@qq.com>
2024-06-12 15:02:03 +0800

    deploy: del nouse component

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -4,7 +4,7 @@ import {
   ExclamationCircleIcon,
 } from '@heroicons/react/24/outline'
 import { ArrowRightIcon } from '@heroicons/react/20/solid'
-import { Button } from './button'
+// import { Button } from './button'
 
 export default function LoginForm() {
   return (
